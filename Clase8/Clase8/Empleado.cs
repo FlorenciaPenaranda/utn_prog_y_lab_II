269638f6095cee9ffed82357b6d58e5bf52d7bbc
FlorenciaPenaranda <florenciaceciliap@gmail.com>
2019-09-16 22:15:21 -0300

    Empresa/empleados

---
+++ Clase8/Clase8/Empleado.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase8
+{
+    public class Empleado
+    {
+        private string nombre; //de instancia?
+        private string apellido;
+        private string legajo;
+        private EPuestoJerarquico puesto;
+        private int salario;
+
+        public Empleado(string nombre, string apellido, string legajo, EPuestoJerarquico puesto, int salario)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.puesto = puesto;
+            this.salario = salario;
+        }
+
+        public enum EPuestoJerarquico
+        {
+            Administración = 1,
+            Gerencia = 2,
+            Sistemas = 3,
+            Accionista = 4,
+        }
+
+        public static bool operator ==(Empleado primerEmpleado, Empleado segundoEmpleado)
+        {
+            if (primerEmpleado != null && segundoEmpleado != null)
+            {
+                if (primerEmpleado.salario == segundoEmpleado.salario)
+                    {
+                        return true;
+                    }
+                
+            }
+            return false;
+        }
+
+        public static bool operator !=(Empleado primerEmpleado, Empleado segundoEmpleado)
+        {
+            return !(primerEmpleado.salario == segundoEmpleado.salario);
+        }
+
+        public string Mostrar()
+        {
+
+        }
+    }
+}
---
+++ Clase8/Clase8/Empresa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase8
+{
+    class Empresa
+    {
+        private List<Empleado> nominaEmpleados;
+        private string razonSocial;
+        private string direccion;
+        private float ganancias;
+
+        public Empresa()
+        {
+            this.nominaEmpleados = new List<Empleado>();
+        }
+
+        public Empresa(string razonSocial, string direccion, float ganancias) : this()
+        {
+            this.razonSocial = razonSocial;
+            this.direccion = direccion;
+            this.ganancias = ganancias;
+
+        }
+
+        public static Empresa operator +(Empleado empleado, Empresa empresa)
+        {
+            if (empleado != null && empresa != null)
+            {
+                foreach (Empleado item in empresa.nominaEmpleados)
+                {
+                    if(item == empleado)
+                    {
+                       return empresa;
+                    }
+                }
+                empresa.nominaEmpleados.Add(empleado);
+                return empresa;
+
+            }//si algun aprametro en null, devolver la empresa.
+            
+        }
+
+  
+    }
+
+
+}

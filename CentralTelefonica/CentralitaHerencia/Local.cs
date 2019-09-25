84780b4c9d9b58bc5e95b8d2db4b17ed3f114fca
FlorenciaPenaranda <florenciaceciliap@gmail.com>
2019-09-24 21:59:17 -0300

    Comienzo de centralita

---
+++ CentralTelefonica/CentralitaHerencia/Local.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Local : Llamada
+    {
+        protected float costo;
+
+        public float CostoLlamadaLocal
+        {
+            get { return CalcularCosto(); }
+        }
+
+        public Local(Llamada llamada, float costo)
+            : base(llamada.Duracion,  llamada.NroDestino, llamada.NroORigen)
+        {
+            this.costo = costo;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder str = new StringBuilder();
+            str.AppendFormat("Duracion: {o}\nNro destino: {1}\nNro origen: {2}\nCosto: {3}", base.duracion, base.nroDestino, base.nroORigen, this.CostoLlamadaLocal);
+            return str.ToString();
+        }
+
+        private float CalcularCosto()
+        {
+            return base.Duracion * this.costo;
+        }
+    }
+}
---
+++ CentralTelefonica/CentralitaHerencia/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

fddb1acb2713834b0392a36f421ef0f874c57899
Florencia Peñaranda <fpenaranda@rsuocra.local>
2019-10-16 18:26:37 -0300

    Ejec. 40

---
+++ centralita/Centralita/Llamada.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centralita
+{
+    public abstract class Llamada
+    {
+        protected float duracion;
+        protected string nroOrigen;
+        protected string nroDestino;
+
+        public float Duracion
+        {
+            get { return this.duracion; }
+        }
+        public string NroDestino
+        {
+            get { return this.nroDestino; }
+        }
+        public string NroOrigen
+        {
+            get { return this.nroOrigen; }
+        }
+
+        public abstract float CostoLlamada { get; }
+
+        public Llamada(float duracion, string nroOrigen, string nroDestino)
+        {
+            this.duracion = duracion;
+            this.nroOrigen = nroOrigen;
+            this.nroDestino = nroDestino;
+        }
+
+        protected virtual string Mostrar() 
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Duracion de la llamada: " + this.duracion);
+            sb.AppendLine("Numero de origen: " + this.nroOrigen);
+            sb.AppendLine("Numero destino: " + this.nroDestino);
+            return sb.ToString();
+        }
+
+        public bool operator ==(Llamada l1, Llamada l2)
+        {
+            if(l1)
+            
+        }
+
+        public bool operator !=(Llamada l1, Llamada l2)
+        {
+
+        }
+
+
+    }
+}
---
+++ centralita/CentralitaHerencia/Provincial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Provincial: Llamada
+    {
+        Franja franjaHoraria;
+
+        public enum Franja
+        {
+            Franja_1,
+            Franja_2,
+            Franja_3
+        }
+
+        public float CostoLlamadaProvincial
+        {
+            get { return CalcularCosto(); }
+        }
+
+        public Provincial(Llamada llamada,Franja franjaHoraria)
+            : base(llamada.Duracion, llamada.NroDestino, llamada.NroORigen)
+        {
+            this.franjaHoraria = franjaHoraria;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder str = new StringBuilder();
+            str.AppendFormat("Duracion: {o}\nNro destino: {1}\nNro origen: {2}\nCosto: {3}", base.duracion, base.nroDestino, base.nroORigen, this.CostoLlamadaProvincial);
+            return str.ToString();
+        }
+
+        private float CalcularCosto()
+        {
+            switch (franjaHoraria)
+            {
+                case 0:
+                    return base.Duracion * 0.99f;
+                    break;
+                case 1:
+                    return base.Duracion * 1.25f;
+                    break;
+                case 3:
+                    return base.Duracion * 0.66f;
+                    break;
+            }
+               
+
+        }
+
+
+    }
+}

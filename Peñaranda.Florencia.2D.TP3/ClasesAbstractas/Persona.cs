b8dfad8913694d2f6fdd4a47201357f481c49ef4
alumno <alumno@utnfra.com.ar>
2019-11-04 22:13:22 -0300

    principio TP3

---
+++ Peñaranda.Florencia.2D.TP3/ClasesAbstractas/Persona.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    abstract class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+
+        public enum ENacionalidad
+        {
+            Argentino,
+            extranjero
+        }
+
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = value;
+            }
+        }
+
+        public int Dni
+        {
+            get
+            {
+                return this.dni;
+            }
+            set
+            {
+                this.dni = value;
+            }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get
+            {
+                return this.nacionalidad;
+            }
+            set
+            {
+                this.nacionalidad = value;
+            }
+        }
+
+        public string StringToDni
+        {
+            set
+            {
+                this.dni = int.Parse(value);
+            }
+        }
+
+        public Persona()
+        {
+
+        }
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.nacionalidad = nacionalidad;
+        }
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+            : this(nombre, apellido, nacionalidad)
+        {
+            this.dni = dni;
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : this(nombre, apellido, nacionalidad)
+        {
+            this.dni = int.Parse(dni);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Nombre: " + this.Nombre);
+            sb.AppendLine("Apellido: " + this.Apellido);
+            sb.AppendLine("Dni: " + this.Dni);
+            sb.AppendLine("Nacionalidad: " + this.Nacionalidad);
+            return sb.ToString();
+        }
+
+        protected int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+           
+        }
+
+        protected int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+
+        }
+
+        protected string ValidarNombreApellido(string dato)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Peñaranda.Florencia.2D.TP3/PruebasUnitarias/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasUnitarias")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("PruebasUnitarias")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5cf91400-f0f7-4c89-926a-36598d6d0964")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
